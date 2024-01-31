ff8866733025c3a9f2f5c3f2fe6da7ca9f04560e
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-01-31 15:02:38 +0100

    WIP: refactor

---
+++ src/lib/Api.py
@@ -0,0 +1,51 @@
+import yaml
+import sys
+
+from lib.IiifManifestGenerator import IiifManifestGenerator
+from lib.DataConnector import FieldConnector
+
+class Api:
+
+    def __init__(self, configYmlPath: str, sparqlEndpoint: str):
+        # Read the configuration YAML file
+        with open(configYmlPath, 'r') as f:
+            self.config = yaml.safe_load(f)
+
+        required = {
+            "fieldDefinitionsFile": "string",
+            "namespaces": {
+                "entities": "string",
+                "manifests": "string"
+            },
+            "queries": {
+                "label": "string",
+                "images": "string"
+            }
+        }
+
+        # Check that the configuration file contains the required parameters
+        for key in required.keys():
+            if key not in self.config.keys():
+                print(f"Error: Missing required parameter '{key}' in configuration file '{configYmlPath}'", file=sys.stderr)
+                sys.exit(1)
+            if isinstance(required[key], dict):
+                for subkey in required[key].keys():
+                    if not self.config[key].keys() and subkey not in self.config[key].keys():
+                       print(f"Error: Missing required parameter '{key}.{subkey}' in configuration file '{configYmlPath}'", file=sys.stderr)
+                       sys.exit(1)
+
+        self.manifest = IiifManifestGenerator(baseUri=self.config['namespaces']['manifests'])
+        self.connector = FieldConnector(sparqlEndpoint=sparqlEndpoint)
+        self.connector.loadFieldDefinitionsFromFile(self.config['fieldDefinitionsFile'])
+
+    def getManifest(self, *, type: str, id: str) -> dict:
+        subject = f"{self.config['namespaces']['entities']}{type}/{id}"
+        manifestId = f"{type}/{id}"
+        label, metadata, images = self.getDataForSubject(subject)
+        return self.manifest.generate(id=manifestId, label=label, images=images, metadata=metadata)
+
+    def getDataForSubject(self, subject: str) -> dict:
+        label = self.connector.getLabelForSubject(subject)
+        metadata = self.connector.getMetadataForSubject(subject)
+        images = self.connector.getImagesForSubject(subject)
+        return label, metadata, images
--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
 """
-Entry point for the FastAPI application. Configuration is done via environment variables.
+Entry point for the IIIF Manifest api.
 
-SPARQL_ENDPOINT: 
-    The endpoint for the SPARQL service to query data from.
+The main configuration is done via a YAML file. The path to the file is passed via the
+CONFIG_YML environment variable.
 
-NAMESPACE_ENTITIES: 
-    The namespace for entities. This is used to construct the URI for the subject based on 
-    a type and ID. For example, for entities of the form 'http://example.com/object/123', 
-    the namespace would be 'http://example.com/', the type would be 'object', and the ID
-    would be '123'.
+The SPARQL endpoint is passed via the SPARQL_ENDPOINT environment variable.
 
-NAMESPACE_MANIFESTS:
-    The namespace for manifests. This is used to construct the URI for the manifest based
-    on a type and ID. For example, for manifests of the form 'http://iiif.example.com/manifest/object/123',
-    the namespace would be 'http://iiif.example.com/', the type would be 'object', and the ID
-
-FIELD_DEFINITIONS_YML:
-    The path to the YAML file containing field definitions. It uses the sari-field-definitions-generator
-    format.
 
 To run the application, use a command like 'uvicorn main:app'.
 """
@@ -27,8 +15,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from lib.IiifManifestGenerator import IiifManifestGenerator
-from lib.DataConnector import FieldConnector
+from lib.Api import Api
 
 app = FastAPI()
 
@@ -43,16 +30,9 @@ app.add_middleware(
 
 # Inititialise parameters
 SPARQL_ENDPOINT = os.environ['SPARQL_ENDPOINT']
-NAMESPACE_ENTITIES = os.environ['NAMESPACE_ENTITIES']
-NAMESPACE_MANIFESTS = os.environ['NAMESPACE_MANIFESTS']
-FIELD_DEFINITIONS_YML = os.environ['FIELD_DEFINITIONS_YML']
+CONFIG_YML = os.environ['CONFIG_YML']
 
-# Initialise manifest generator and data connector
-manifest = IiifManifestGenerator(baseUri=NAMESPACE_MANIFESTS)
-connector = FieldConnector(sparqlEndpoint=SPARQL_ENDPOINT)
-
-# Load field definitions
-connector.loadFieldDefinitionsFromFile(FIELD_DEFINITIONS_YML)
+api= Api(CONFIG_YML, SPARQL_ENDPOINT)
 
 @app.get("/", response_class=HTMLResponse)
 def readRoot():
@@ -70,16 +50,4 @@ def readRoot():
 
 @app.get("/manifest/{item_type}/{item_id}")
 def getManifest(item_type: str, item_id: str):
-    return _getManifest(type=item_type, id=item_id)
-
-def _getManifest(*, type: str, id: str) -> dict:
-    subject = f"{NAMESPACE_ENTITIES}{type}/{id}"
-    manifestId = f"{type}/{id}"
-    label, metadata, images = _getDataForSubject(subject)
-    return manifest.generate(id=manifestId, label=label, images=images, metadata=metadata)
-
-def _getDataForSubject(subject: str) -> dict:
-    label = connector.getLabelForSubject(subject)
-    metadata = connector.getMetadataForSubject(subject)
-    images = connector.getImagesForSubject(subject)
-    return label, metadata, images
+    return api.getManifest(type=item_type, id=item_id)
